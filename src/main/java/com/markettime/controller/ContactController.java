17240c32daff81f0ad8c437ef8998e53d810055d
Dana Onisa <dana.onisa@garmin.com>
2016-03-22 21:37:06 +0200

    added contact controller

---
+++ src/main/java/com/markettime/controller/ContactController.java
@@ -0,0 +1,28 @@
+package com.markettime.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.markettime.model.dto.MessageDto;
+
+/**
+ * 
+ * @author Dana Onisa
+ *
+ */
+@Controller
+@RequestMapping("/contact")
+public class ContactController extends BaseController {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String contact(){
+		return "contact";
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public void sendEmail(@ModelAttribute MessageDto messageDto){
+		System.out.println(messageDto);
+	}
+}
